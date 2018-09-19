9cd10edcb3d97f1245aa56b5a3cc1bc5f3db95c1
lan <adwerrd@hotmail.com>
2018-09-19 19:12:41 +0800

    Build release

---
+++ conf.js
@@ -0,0 +1,13 @@
+// shit
+window.domain = 'baishancloud.com'
+window.dashboard_conf = {
+  apiHost: '47.93.3.45:9090',
+  awsHost: '47.93.3.45',
+  ssoHost: 'test-sso.bs58i.baishancloud.com',
+  transcoderHOST: 'transcoder-ss.bscstorage.com',
+  onlineMode: 'True',
+  upgradeMode: 'False',
+  appID: '6',
+  previewAk: 'acc_drdrxp',
+  previewSk: '11111111111111111111',
+}
